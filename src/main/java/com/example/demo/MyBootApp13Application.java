292575a680299d35a0bff30e9c3dd06ed608534b
panka <panka@192.168.11.3>
2020-10-06 11:22:33 +0900

    エンティティのCRUD

---
+++ src/main/java/com/example/demo/MyBootApp13Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApp13Application.class, args);
+	}
+
+}
